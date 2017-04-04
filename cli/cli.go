0a7773d3b3e609e35d6641c1942282aa4e4d94a7
brotherlogic <brotherlogic@gmail.com>
2017-04-03 20:50:13 -0700

    Added a cli for building. This closes #7.

---
+++ cli/cli.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pbdi "github.com/brotherlogic/discovery/proto"
+	pb "github.com/brotherlogic/gobuildslave/proto"
+)
+
+func findServer(name, server string) (string, int) {
+	conn, _ := grpc.Dial("192.168.86.34:50055", grpc.WithInsecure())
+	defer conn.Close()
+
+	registry := pbdi.NewDiscoveryServiceClient(conn)
+	rs, _ := registry.ListAllServices(context.Background(), &pbdi.Empty{})
+
+	for _, r := range rs.Services {
+		if r.Identifier == server && r.Name == name {
+			return r.Ip, int(r.Port)
+		}
+	}
+
+	return "", -1
+}
+
+func main() {
+	buildFlags := flag.NewFlagSet("BuildServer", flag.ExitOnError)
+	var name = buildFlags.String("name", "", "Name of the binary to build")
+	var server = buildFlags.String("server", "", "Name of the server to build on")
+
+	if len(os.Args) <= 1 {
+		fmt.Printf("Commands: build\n")
+	} else {
+		switch os.Args[1] {
+		case "build":
+			if err := buildFlags.Parse(os.Args[2:]); err == nil {
+				host, port := findServer("gobuildslave", *server)
+
+				conn, _ := grpc.Dial(host+":"+strconv.Itoa(port), grpc.WithInsecure())
+				defer conn.Close()
+
+				registry := pb.NewGoBuildSlaveClient(conn)
+				_, err := registry.BuildJob(context.Background(), &pb.JobSpec{Name: *name})
+				if err != nil {
+					log.Printf("Error building job: %v", err)
+				}
+			}
+		}
+	}
+}
