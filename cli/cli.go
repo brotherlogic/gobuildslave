a0be33b64fa2ff399c9d43ec5ab32f87faccbd9f
Simon Tucker <brotherlogic@gmail.com>
2018-01-14 14:39:16 -0800

    Adds support for registering that a server supports CDs

--- cli/cli.go
+++ cli/cli.go
@@ -35,6 +35,23 @@ func findServer(name, server string) (string, int) {
 	return "", -1
 }
 
+func findServers() []*pbdi.RegistryEntry {
+	conn, _ := grpc.Dial(utils.Discover, grpc.WithInsecure())
+	defer conn.Close()
+
+	registry := pbdi.NewDiscoveryServiceClient(conn)
+	rs, _ := registry.ListAllServices(context.Background(), &pbdi.Empty{})
+
+	list := make([]*pbdi.RegistryEntry, 0)
+	for _, r := range rs.Services {
+		if r.Name == "gobuildserver" {
+			list = append(list, r)
+		}
+	}
+
+	return list
+}
+
 func main() {
 	buildFlags := flag.NewFlagSet("BuildServer", flag.ExitOnError)
 	var name = buildFlags.String("name", "", "Name of the binary to build")
@@ -97,6 +114,22 @@ func main() {
 				}
 				for _, r := range res.Details {
 					fmt.Printf("%v (%v) - %v\n", r.Spec.Name, time.Unix(r.StartTime, 0).Format("02/01 15:04"), r)
+				}
+			}
+		case "config":
+			if err := buildFlags.Parse(os.Args[2:]); err == nil {
+				servers := findServers()
+
+				for _, s := range servers {
+					conn, _ := grpc.Dial(s.GetIp()+":"+strconv.Itoa(int(s.GetPort())), grpc.WithInsecure())
+					defer conn.Close()
+
+					registry := pb.NewGoBuildSlaveClient(conn)
+					res, err := registry.GetConfig(context.Background(), &pb.Empty{})
+					if err != nil {
+						log.Fatalf("Error building job: %v", err)
+					}
+					fmt.Printf("%v - %v\n", s.GetIdentifier(), res)
 				}
 			}
 		}
