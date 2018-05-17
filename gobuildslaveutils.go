00a3cdf6eac1afc386fb19d5fe82bc6bfe819865
Simon Tucker <brotherlogic@gmail.com>
2018-05-17 15:48:51 -0700

    Adds a crash report on build. This closes #118.

--- gobuildslaveutils.go
+++ gobuildslaveutils.go
@@ -23,10 +23,16 @@ func (s *Server) runTransition(job *pb.JobAssignment) {
 			job.State = pb.State_BUILT
 		}
 	case pb.State_BUILT:
-		key := s.scheduleRun(job.Job)
-		job.CommandKey = key
-		job.StartTime = time.Now().Unix()
-		job.State = pb.State_PENDING
+		output := s.scheduler.getOutput(job.CommandKey)
+		if len(output) > 0 {
+			s.deliverCrashReport(job, output)
+			job.State = pb.State_DIED
+		} else {
+			key := s.scheduleRun(job.Job)
+			job.CommandKey = key
+			job.StartTime = time.Now().Unix()
+			job.State = pb.State_PENDING
+		}
 	case pb.State_PENDING:
 		if time.Now().Add(-time.Minute).Unix() > job.StartTime {
 			job.State = pb.State_RUNNING
