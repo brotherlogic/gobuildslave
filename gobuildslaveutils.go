4697145867ccc26fa13b26d44f625e9f0e943f81
Simon Tucker <simon@brotherlogic@gmail>
2018-10-29 09:07:11 -0700

    Add died

--- gobuildslaveutils.go
+++ gobuildslaveutils.go
@@ -86,6 +86,7 @@ func (s *Server) runTransition(ctx context.Context, job *pb.JobAssignment) {
 			}
 		}
 	case pb.State_DIED:
+		s.stateMap[job.Job.Name] = fmt.Sprintf("DIED %v", job.CommandKey)
 		s.scheduler.removeJob(job.CommandKey)
 		job.State = pb.State_ACKNOWLEDGED
 	}
