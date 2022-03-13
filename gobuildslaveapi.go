2d8f56dd1269c22d5f638e4aca2888facd3aeaf3
Simon Tucker <brotherlogic@gmail.com>
2022-03-13 10:57:28 -0700

    Support bit running

--- gobuildslaveapi.go
+++ gobuildslaveapi.go
@@ -16,6 +16,9 @@ import (
 
 // RunJob - runs the job
 func (s *Server) RunJob(ctx context.Context, req *pb.RunRequest) (*pb.RunResponse, error) {
+	if s.Bits != int(req.GetBits()) {
+		return &pb.RunResponse{}, status.Errorf(codes.FailedPrecondition, "Cannot run %v bits on this server", req.GetBits())
+	}
 	if !s.doesBuild && !req.Job.Breakout {
 		return &pb.RunResponse{}, status.Errorf(codes.FailedPrecondition, "Refusing to build")
 	}
